0bd32479c5dc13748db5fb76ac4333b219e9d06d
arumayara <72974847+arumayara@users.noreply.github.com>
2020-10-19 07:19:34 +0700

    Add files via upload

---
+++ PBO10K-10119905-Latihan15-Logika/src/main/java/PBO10K/NIM10119905/Latihan15/Logika/PBO10K10119905Latihan15.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO10K.NIM10119905.Latihan15.Logika;
+
+/**
+ *
+ * @author 
+ * NAMA    : Abraham Rumayara
+ * KELAS   : PBO10K
+ * NIM     : 10119905
+ * Deskripsi Program : Logika
+ */
+public class PBO10K10119905Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
